dcbe7e4c58c447392c1aab59572595085f062b6b
Your Name <you@example.com>
2022-07-07 17:20:05 -0400

    work in progress

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -1,8 +1,10 @@
 package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.AccountDao;
+import com.techelevator.tenmo.dao.TransferDao;
 import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.Balance;
+import com.techelevator.tenmo.model.Transfer;
 import com.techelevator.tenmo.model.User;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import java.util.List;
 public class AccountController {
     private AccountDao accountDao;
     private UserDao userDao;
+    private TransferDao transferDao;
     private final static String BASE_API_URL = "http://localhost:8080/";
 
     public AccountController(AccountDao accountDao, UserDao userDao) {
@@ -31,6 +34,11 @@ public class AccountController {
     @RequestMapping(path = "usernames", method = RequestMethod.GET)
     public List<User> users(){
         return userDao.userNames();
+    }
+
+    @RequestMapping(path ="user/{id}/transfers/{transferid}", method = RequestMethod.GET )
+    public Transfer transfer(@PathVariable int id){
+        return transferDao.getTransferByTransferId(id);
     }
 
 }
